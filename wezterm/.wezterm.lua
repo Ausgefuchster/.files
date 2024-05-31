3ebc58bd6019de6737ce12a5294b0cc3b85cda7c
Ausgefuchster <51484658+Ausgefuchster@users.noreply.github.com>
2024-05-31 22:26:15 +0200

    configs(wezterm): add keybindings to handle workspaces

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
-local act = wezterm.action
+local action = wezterm.action
 
-config.default_prog = { 'pwsh' }
+config.default_prog = { 'pwsh', '-NoLogo' }
 
 config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = 'INTEGRATED_BUTTONS'
@@ -14,15 +14,35 @@ config.color_scheme = 'rose-pine'
 
 config.keys = {
     {
-        key = 'R',
+        key = 'r',
         mods = 'CTRL|SHIFT',
-        action = act.PromptInputLine({
+        action = action.PromptInputLine({
             description = 'New name for tab',
             action = wezterm.action_callback(function(window, _, text)
                 window:active_tab():set_title(text)
             end),
         })
     },
+    {
+        key = 'w',
+        mods = 'CTRL|SHIFT|ALT',
+        action = action.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }),
+    },
+    {
+        key = '{',
+        mods = 'CTRL|SHIFT',
+        action = action.SwitchWorkspaceRelative(-1)
+    },
+    {
+        key = '}',
+        mods = 'CTRL|SHIFT',
+        action = action.SwitchWorkspaceRelative(1),
+    },
+    {
+        key = 'x',
+        mods = 'CTRL|SHIFT|ALT',
+        action = action.CloseCurrentPane({ confirm = true }),
+    }
 }
 
 return config
